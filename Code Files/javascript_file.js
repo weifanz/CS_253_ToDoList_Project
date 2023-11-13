a22e1f0c3e933f2840cc449743c710fa647b5d59
Samuel Okewole <sokewole@iwu.edu>
2023-11-13 13:08:58 -0600

    js and unordered list addition

--- Code Files/javascript_file.js
+++ Code Files/javascript_file.js
@@ -1,3 +1,21 @@
+/* FILE: main.js
+* --------------
+*/
+
+// Buttons from html file
+const task_btn = document.querySelector('Add_task_btn');
+const select_btn = document.querySelector('select_day_btn');
+
+task_btn.addEventListener('Click', () => {
+    /*
+    * task_btn.addEventListener is a function that listens for a click and runs the function
+    * The function will take in the input and store it in the 
+    */
+});
+
+
+
+
 function addToColumn(add_day, add_task) {
     var cellId = add_day.toLowerCase();
     var cell = document.getElementById(cellId);
