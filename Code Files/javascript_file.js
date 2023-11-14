61417610486a75def67e6243338f52a46e1e658d
weifanz <115199657+weifanz@users.noreply.github.com>
2023-11-14 14:17:52 -0600

    Revise the code to "add" tasks

--- Code Files/javascript_file.js
+++ Code Files/javascript_file.js
@@ -2,52 +2,29 @@
 * --------------
 */
 
+document.addEventListener("DOMContentLoaded", function() {
 // Buttons from html file
-const task_btn = document.querySelector('add_task_btn');
-const select_btn = document.querySelector('select_day_btn');
+const task_btn = document.getElementById('add_task_btn');
+const add_day = document.getElementById('add_day');
+const add_task = document.getElementById('add_task');
 
-task_btn.addEventListener('click', () => {
-    /*
-    * task_btn.addEventListener is a function that listens for a click and runs the function
-    * The function will take in the input and store it in the correct list
-    */
-    var day = document.getElementById(add_day).value;
-    var content = document.getElementById(add_task).value; // Getting user input
-    if (day.toLowerCase() == 'sunday') {
-        const listTask = document.createElement('li'); 
-        listTask.value = content;
-        sunday.appendChild(listTask);
-    }
-    else if (add_day.toLowerCase() == 'monday') {
-        const listTask = document.createElement('li');
-    }
-    else if (add_day.toLowerCase() == 'tuesday') {
-        const listTask = document.createElement('li');
-    }
-    else if (add_day.toLowerCase() == 'wednesday') {
-        const listTask = document.createElement('li');
-    }
-    else if (add_day.toLowerCase() == 'thursday') {
-        const listTask = document.createElement('li');
-    }
-    else if (add_day.toLowerCase() == 'friday') {
-        const listTask = document.createElement('li');
-    }
-    else if (add_day.toLowerCase() == 'saturday') {
-        const listTask = document.createElement('li');
-    }
-    else {
-        alert("Not a valid day of the week :(");
-    }
-});
+    task_btn.addEventListener("click", function(event) {
+        /*
+        * task_btn.addEventListener is a function that listens for a click and runs the function
+        * The function will take in the input and store it in the correct list
+        */
+        var content = add_task.value; // Getting user input
 
+        if (content != "") {
+            var parent = document.getElementById(add_day.value);
 
+            var listTask = document.createElement('li'); 
+            listTask.textContent = content;
+            
+            parent.appendChild(listTask);
+        } 
+        
+        add_task.value = '';
+    });
 
-
-function addToColumn(add_day, add_task) {
-    var cellId = add_day.toLowerCase();
-    var cell = document.getElementById(cellId);
-    if (cell) {
-        cell.textContent = add_task;
-    }
-}
+})
