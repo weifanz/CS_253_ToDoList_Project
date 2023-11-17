b6db1a38d2996565a35eeb6e20ec10be61fe32e1
Jordan Pierce <jpierce2@iwu.edu>
2023-11-17 12:18:52 -0600

    added checkboxes function

--- code_files/main.js
+++ code_files/main.js
@@ -17,16 +17,66 @@ const add_task = document.getElementById('add_task');
 
         if (content != "") {
             var parent = document.getElementById(add_day.value);
-
+            
             var listTask = document.createElement('li'); 
             listTask.textContent = content;
             
             parent.appendChild(listTask);
+            createCheckbox();
         } else {
             alert("Please add your task.");
         }
-        
+
         add_task.value = '';
     });
+
+    // Function to handle checkbox state change
+    function handleCheckboxChange(checkbox) {
+        if (checkbox.checked) {
+          
+       
+        } else {
+          
+         
+        }
+      }
+  
+      // Function to create a checkbox
+      function createCheckbox() {
+        // Get the container element
+        var container = document.body;
+  
+        // Checkbox value
+        var checked = false;
+  
+        // Create checkbox dynamically
+        checked.forEach(function(value) {
+          // Create a checkbox element
+          var checkbox = document.createElement('input');
+          checkbox.type = 'checkbox';
+          checkbox.value = false;
+  
+          // Set a unique ID for each checkbox
+          checkbox.id = 'checkbox_' + content;
+  
+          // Create a label for the checkbox
+          var label = document.createElement('label');
+          label.appendChild(document.createTextNode(value));
+          label.setAttribute('for', checkbox.id);
+  
+          // Add an event listener for the checkbox
+          checkbox.addEventListener('change', function() {
+            handleCheckboxChange(checkbox);
+          });
+  
+          // Append the checkbox and label to the container
+          container.appendChild(checkbox);
+          container.appendChild(label);
+          container.appendChild(document.createElement('br')); // Line break for better spacing
+        });
+      }
+  
+     
+      
 
 })
