40e70257b91d4d093183e8235483ea798b283e6f
Samuel Okewole <sokewole@iwu.edu>
2023-12-01 12:48:43 -0600

    Readability and remove checkbox to javascript file

--- code_files/main.js
+++ code_files/main.js
@@ -3,96 +3,51 @@
 */
 
 document.addEventListener("DOMContentLoaded", function() {
-// Buttons from html file
-const task_btn = document.getElementById('add_task_btn');
-const add_day = document.getElementById('add_day');
-const add_task = document.getElementById('add_task');
-const deleteBtn = document.getElementById('delete_task_btn');
-    task_btn.addEventListener("click", function(event) {
-        /*
-        * task_btn.addEventListener is a function that listens for a click and runs the function
-        * The function will take in the input and store it in the correct list
-        */
-        var content = add_task.value; // Getting user input
 
-        if (content != "") {
-            var parent = document.getElementById(add_day.value);
-            
-            var listTask = document.createElement('li'); 
-            listTask.textContent = content;
-            
-            parent.appendChild(listTask);
-            createCheckbox();
-        } else {
-            alert("Please add your task.");
-        }
+  // Buttons from html file
+  const task_btn = document.getElementById('add_task_btn');
+  const add_day = document.getElementById('add_day');
+  const add_task = document.getElementById('add_task');
+  const deleteBtn = document.getElementById('delete_task_btn');
+    
+  task_btn.addEventListener("click", function(event) {
+  /*
+  * task_btn.addEventListener is a function that listens for a click and runs the function
+  * The function will take in the input and store it in the correct list
+  */
+  var content = add_task.value; // Getting user input
 
-        add_task.value = '';
+  if (content != "") {
+    var parent = document.getElementById(add_day.value);
+            
+    var listTask = document.createElement('li'); 
+    listTask.textContent = content;
+            
+    parent.appendChild(listTask);
+    createCheckbox();
+    } 
+    
+    else {
+      alert("Please add your task.");
+    }
+
+    add_task.value = '';
     });
 
+  deleteBtn.addEventListener("click", function(event) {
     /*
     this function will act as the delete function which will allow the user to remove a specified task
     */
-deleteBtn.addEventListener("click", function(event){
-  var contentToDelete = add_task.value;
-  var dayToDeleteFrom = add_day.value;
-  var listItems = document.getElementById(dayToDeleteFrom).getElementsByTagName('li');
+    var contentToDelete = add_task.value;
+    var dayToDeleteFrom = add_day.value;
+    var listItems = document.getElementById(dayToDeleteFrom).getElementsByTagName('li');
 
-  for (var i = 0; i < listItems.length; i++){
-    if (listItems[i].textContent === contentToDelete){
-      listItems[i].remove();
-      break; // exit the loop once the task is deleted
+    for (var i = 0; i < listItems.length; i++){
+      if (listItems[i].textContent === contentToDelete){
+        listItems[i].remove();
+        break; // exit the loop once the task is deleted
+      }
     }
-  }
-})
-
-    // Function to handle checkbox state change
-    function handleCheckboxChange(checkbox) {
-        if (checkbox.checked) {
-          
-       
-        } else {
-          
-         
-        }
-      }
-  
-      // Function to create a checkbox
-      function createCheckbox() {
-        // Get the container element
-        var container = document.body;
-  
-        // Checkbox value
-        var checked = false;
-  
-        // Create checkbox dynamically
-        checked.forEach(function(value) {
-          // Create a checkbox element
-          var checkbox = document.createElement('input');
-          checkbox.type = 'checkbox';
-          checkbox.value = false;
-  
-          // Set a unique ID for each checkbox
-          checkbox.id = 'checkbox_' + content;
-  
-          // Create a label for the checkbox
-          var label = document.createElement('label');
-          label.appendChild(document.createTextNode(value));
-          label.setAttribute('for', checkbox.id);
-  
-          // Add an event listener for the checkbox
-          checkbox.addEventListener('change', function() {
-            handleCheckboxChange(checkbox);
-          });
-  
-          // Append the checkbox and label to the container
-          container.appendChild(checkbox);
-          container.appendChild(label);
-          container.appendChild(document.createElement('br')); // Line break for better spacing
-        });
-      }
-  
-     
-      
+  });
 
 })
