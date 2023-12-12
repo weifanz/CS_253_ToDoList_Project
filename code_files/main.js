ce0c574f77a38e44bca3a38d80f0b52c598dab17
weifanz <115199657+weifanz@users.noreply.github.com>
2023-12-12 11:28:39 -0600

    Update the function of updating delete taskbar

--- code_files/main.js
+++ code_files/main.js
@@ -5,37 +5,65 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // buttons from html file
-  const task_btn = document.getElementById('add_task_btn');
-  const add_day = document.getElementById('add_day');
-  const add_task = document.getElementById('add_task');
+  const addTaskBtn = document.getElementById('add_task_btn');
+  const addDay = document.getElementById('add_day');
+  const addTask = document.getElementById('add_task');
 
   const deleteBtn = document.getElementById('delete_task_btn');
   const deleteDay = document.getElementById('delete_day');
   const chooseTask = document.getElementById('choose_task');
+
+  let tasks = [];
+
+  class Task {
+    constructor(day, taskContent) {
+      this.day = day;
+      this.taskContent = taskContent;
+    }
+  }
+
+  // UpdateDeleteDropdown Function
+  function updateDeleteDropdown() {
+    chooseTask.innerHTML = ""; // Clear existing options
+    var selectedDay = deleteDay.value;
+    var dayTasks = tasks.filter(task => task.day === selectedDay);
+    dayTasks.forEach(task => {
+        var option = document.createElement('option');
+        option.value = task.taskContent;
+        option.textContent = task.taskContent;
+        chooseTask.appendChild(option);
+    });
+  }
     
-  task_btn.addEventListener("click", function(event) {
+  addTaskBtn.addEventListener("click", function(event) {
   /*
   * task_btn.addEventListener is a function that listens for a click and runs the function
   * The function will take in the input and store it in the correct list
   */
-  var content = add_task.value; // Getting user input
+  var content = addTask.value; // Getting user input
 
   if (content != "") {
-    var parent = document.getElementById(add_day.value);
+    let day = addDay.value;
+    let task = new Task(day, content);
+    tasks.push(task)
+
+    var parent = document.getElementById(day);
     var listTask = document.createElement('li'); 
+
     listTask.textContent = content;
     parent.appendChild(listTask);
-    createCheckbox();
-    DeleteDropDown();
+    addTask.value = '';
+
+    updateDeleteDropdown();
     } 
     
     else {
       alert("Please add your task.");
     }
 
-    add_task.value = '';
     });
 
+    // Delete Button Event Listener
     deleteBtn.addEventListener("click", function(event) {
       var contentToDelete = chooseTask.value;
       var dayToDeleteFrom = deleteDay.value;
@@ -43,26 +71,17 @@ document.addEventListener("DOMContentLoaded", function() {
       for (var i = 0; i < listItems.length; i++) {
           if (listItems[i].textContent === contentToDelete) {
               listItems[i].remove();
+              // Update the tasks array to remove the deleted task
+              tasks = tasks.filter(task => !(task.day === dayToDeleteFrom && task.taskContent === contentToDelete));
               break;
           }
       }
-      DeleteDropdown();
-  });
+      updateDeleteDropdown();
+    });
+    
 
-  function createCheckbox() {
-      // function to create a checkbox
-  }
-
-  function DeleteDropdown() {
-    chooseTask.innerHTML = ""; // clear existing options
-    var dayToDeleteFrom = deleteDay.value;
-    var listItems = document.getElementById(dayToDeleteFrom).getElementsByTagName('li');
-    for (var i = 0; i < listItems.length; i++) {
-        var option = document.createElement('option');
-        option.value = listItems[i].textContent;
-        option.textContent = listItems[i].textContent;
-        chooseTask.appendChild(option);
-    }
-}
-
+    deleteDay.addEventListener("change", function(event) {
+      updateDeleteDropdown();
+    });
+    
 });
